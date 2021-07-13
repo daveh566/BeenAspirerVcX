2f57daa03c2880277a1b9e6b5b68db20fe3559e1
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-07-13 16:23:03 +0300

    Update admins.py

--- admins.py
+++ admins.py
@@ -13,3 +13,27 @@ def get(chat_id: int) -> List[int]:
     if chat_id in admins:
         return admins[chat_id]
     return []
+from typing import List
+
+import Chat
+
+import get as gett
+import set
+
+
+async def get_administrators(chat: Chat) -> List[int]:
+    get = gett(chat.id)
+
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
+
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
+
+        set(chat.id, to_set)
+        return await get_administrators(chat)
+
