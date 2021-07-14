95fa3edf9566c3c441c23b061a2661b405e1f7f1
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-07-14 17:40:52 +0300

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -10,4 +10,11 @@ from queues import put
 from queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
+from queues import clear 
+from queues import get
+from queues import is_empty
+from queues import put
+from queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
 
